95749a02ea64a3a6d5a94624525319ddbaeebbc9
nayejun626 <skdpwns506@naver.com>
2023-09-14 14:19:00 +0900

    first commit

---
+++ practice:/week2/test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int a = 10;
+    int b = 15;
+    printf("%d\n", a + b);
+
+    char text1[10] = "hello";
+    char text2[50] = "world";
+
+    printf("%s!\n", text1);
+    printf("%s!\n", text2);
+
+    return 0;
+}
