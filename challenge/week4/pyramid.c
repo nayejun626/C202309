62f8cc7709d79662592cc072b3aff1ac67c91497
nayejun626 <skdpwns506@naver.com>
2023-09-25 15:42:13 +0900

    week4.1.반복문실습+피라미드쌓기

---
+++ challenge/week4/pyramid.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int floor;                              // 사용자로부터 입력받을 층수
+    printf("몇 층으로 쌓을까요?: (5~100)"); // 5에서 100까지 받기
+    scanf("%d", &floor);
+
+    for (int i = 0; i < floor; i++) // 층수만큼 반복
+    {
+        for (int j = 0; j < floor - 1 - i; j++) // s 출력 (공백대신)
+        {
+            printf("s");
+        }
+        for (int k = 0; k < (i * 2) + 1; k++) // 별 출력
+        {
+            printf("*");
+        }
+        printf("\n"); // 개행 출력
+    }
+    return 0;
+}
