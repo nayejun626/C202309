d1b8901237bf8794ed9fcb2ba8928427a7b48360
nayejun626 <skdpwns506@naver.com>
2023-11-09 12:43:01 +0900

    포인터연습

--- challenge/week10/calculator.c
+++ challenge/week10/calculator.c
@@ -2,24 +2,16 @@
 
 double summation(int *a, int *b);
 double subtraction(int *a, int *b);
-double multification(int *a, int *b);
+double multiplication(int *a, int *b);
 double division(int *a, int *b);
 
 int main()
 {
     double a = 2, b = 3;
-    double result;
-    summation(&a, &b, &result);
-    printf("%.0lf + %.0lf = %.2lf \n", a, b, result);
-
-    subtraction(&a, &b, &result);
-    printf("%.0lf - %.0lf = %.2lf \n", a, b, result);
-
-    multification(&a, &b, &result);
-    printf("%.0lf * %.0lf = %.2lf \n", a, b, result);
-
-    division(&a, &b, &result);
-    printf("%.0lf / %.0lf = %.2lf \n", a, b, result);
+    printf("%.0lf + %.0lf = %.2lf \n", a, b, summation(&a, &b));
+    printf("%.0lf - %.0lf = %.2lf \n", a, b, subtraction(&a, &b));
+    printf("%.0lf * %.0lf = %.2lf \n", a, b, multiplication(&a, &b));
+    printf("%.0lf / %.0lf = %.2lf \n", a, b, division(&a, &b));
 
     return 0;
 }
@@ -28,14 +20,17 @@ double summation(int *a, int *b)
 {
     return *a + *b;
 }
+
 double subtraction(int *a, int *b)
 {
     return *a - *b;
 }
-double multification(int *a, int *b)
+
+double multiplication(int *a, int *b)
 {
     return *a * *b;
 }
+
 double division(int *a, int *b)
 {
     return *a / *b;
